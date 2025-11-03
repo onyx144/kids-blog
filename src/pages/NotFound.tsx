ae61e2f143c8baec8bb4881e40f951bb4d35370e
onyx144 <nesmeyanovnikita@gmail.com>
2025-11-03 22:11:14 +0200

    update: not found 3

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,7 @@
 
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
-/* Not found section this:    */
+/* Not found section this */
 const NotFound = () => {
   const location = useLocation();
 
